ba35eb4ea5756f9c9d64f739b3c7130e99327ab8
marsis <logozynski@gmail.com>
2018-06-22 17:40:46 +0300

    from work

--- app.js
+++ app.js
@@ -4,17 +4,71 @@ var http = require('http');
 var config = require('config');
 var log = require('libs/log')(module);
 
+var router = express.Router();
+
 var app = express();
-app.set('port', config.get('port'));
+
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var logger = require('logger');
+var errorhandler = require('errorhandler')
+
+var cookieParser = require('cookie-parser')
+
+//Middleware
 
 
-http.createServer(app).listen(config.get('port'), function() {
-  log.info('Server listening on port ' + config.get('port'))
+app.engine('ejs', require('ejs-locals')); //layout partial block
+app.set('views', __dirname + '/templates') //папка с шаблонами
+app.set('view engine', 'ejs'); //ejs - шаблонизатор
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/*if (app.get('env') == 'development') {
+    app.use(logger.createLogger('dev')); //res.end
+} else {
+    app.use(logger.createLogger('default'));
+}*/
+
+app.use(bodyParser.json()); //разбирает тело запроса form json, post req.body
+
+app.use(cookieParser())//req.cookies
+
+/*app.use(app.router);
+
+app.get('/', function(req, res, next) {
+    res.end("Test");
+});*/
+
+app.use(router);
+
+router.get('/error', function(req, res, next) {
+    // here we cause an error in the pipeline so we see express-winston in action.
+    return next(new Error("This is an error and it should be logged to the console"));
+});
+
+router.get('/', function(req, res, next) {
+   // res.render('This is a normal request, it should be logged to the console too');
+    res.render("index", {
+
+    });
 });
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Middleware
+
+app.use(function (err, req, res, next) {
+    // NODE_ENV = 'production'
+    if (app.get('env') == 'development') {
+      var errorHandler = express.errorHandler();
+       app.use(errorhandler())
+
+    } else {
+      res.send(500);
+    }
+});
+
+/*
 
 app.use(function(req, res, next) {
   if(req.url == '/') {
@@ -47,3 +101,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+*/
+
+
+http.createServer(app).listen(config.get('port'), function() {
+    log.info('Server listening on port ' + config.get('port'))
+});
+
